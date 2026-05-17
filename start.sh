e39ed19bd7b4ee365b2e89e155ad1ddb466b7432
Moman Amjad <momanamjad07@gmail.com>
2026-05-17 09:36:33 +0500

    fix: disable git pager and set terminal type

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ ! -d "/workspace/project/.git" ]; then
   echo "Cloning GitHub clone project..."
-  git clone https://github.com/momanamjad/github /workspace/project
+  git clone https://${GITHUB_TOKEN}@github.com/momanamjad/github /workspace/project
   echo "Clone complete."
 else
   echo "Project already cloned, pulling latest..."
@@ -12,6 +12,9 @@ fi
 
 export PROJECT_PATH=/workspace/project
 export SERVER_ONLY=true
+export GIT_PAGER=cat
+export PAGER=cat
+export TERM=xterm-256color
 
 echo "Starting github-cli server..."
 exec /usr/local/bin/github-cli
